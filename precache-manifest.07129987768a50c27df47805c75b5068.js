82fefdf6e604b3077011a3d7bd9a5b97abc365a2
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-07-08 15:04:10 +0300

    Updates

---
+++ precache-manifest.07129987768a50c27df47805c75b5068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d2076517cd4595fef889a0259bce89",
+    "url": "/style-editor/index.html"
+  },
+  {
+    "revision": "60acafc31c065ba740f3",
+    "url": "/style-editor/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "fbd1a5868263bf4b3303",
+    "url": "/style-editor/static/css/main.854c18bb.chunk.css"
+  },
+  {
+    "revision": "60acafc31c065ba740f3",
+    "url": "/style-editor/static/js/2.e93beefa.chunk.js"
+  },
+  {
+    "revision": "fbd1a5868263bf4b3303",
+    "url": "/style-editor/static/js/main.cd013a30.chunk.js"
+  },
+  {
+    "revision": "aa41c25a2c57097cec71",
+    "url": "/style-editor/static/js/runtime~main.408c3765.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/style-editor/precache-manifest.c6168737171b06089d8f9447c328c2f8.js"
+  "/style-editor/precache-manifest.07129987768a50c27df47805c75b5068.js"
 );
 
 self.addEventListener('message', (event) => {
